cd6ccbc486cebe1c38bc4ff4edc1d556398bba46
jordanthomp81 <jordanthomp81@gmail.com>
2016-04-06 10:55:43 -0500

    added the custom waypoint file;

---
+++ easy-waypoints.js
@@ -0,0 +1,27 @@
+/*-------------------------------------
+  EASY WAYPOINT FUNCTIONS
+-------------------------------------*/
+// Creates a standerd waypoint with the option of custom logic. To pass in
+// the custom logic, just create a function with all the logic you would
+// like to call when the waypoint is activated, then pass just the name of the
+// function into this function without qoutes. Note that these waypoint functions are
+// available to any js file in this project
+// Example Single Waypoint: createWaypoint('.that', 'is-active', '35%', animateThat)
+function createWaypoint(element, classToToggle, offset, cb) {
+  return jQuery(element).waypoint(function(direction) {
+    jQuery(element).toggleClass(classToToggle);
+    if (typeof cb !== "undefined") {
+      cb(element, classToToggle, offset, direction);
+    }
+  }, { offset: offset });
+}
+
+// A loop for standerd waypoint creation. Also has the ability to pass in custom
+// logic, and classToToggle. Both are optional.
+// Example Multiple Waypoints: waypointer(['.that', '#that', '#this'], 'resolved', '10%', animate);
+function waypointer(elementArray, classToToggle, offset, cb) {
+  for (var i=0; i < elementArray.length; i++) {
+    createWaypoint(elementArray[i], classToToggle, offset, cb);
+  }
+  return true;
+}
